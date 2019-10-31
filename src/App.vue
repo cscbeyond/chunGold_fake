a9571414f58e1a31c626f64a0bed94f7b3ca5e6a
geekchen <cscbeyond@sina.com>
2019-10-31 23:40:30 +0800

    test Jenlins

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,20 @@
 <template>
-    <div id="app">
-        <Header></Header>
-        <router-view />
-    </div>
+  <div id="app">
+    <Header></Header>
+    <router-view />
+  </div>
 </template>
 <script>
-    import Header from './components/Header.vue';
-    export default {
-        mounted() {
-            this.showLoading();
-        },
-        methods: {
-            showLoading() {}
-        },
-        components: {
-            Header,
-        }
-    }
-
+import Header from "./components/Header.vue";
+export default {
+  mounted() {
+    this.showLoading();
+  },
+  methods: {
+    showLoading() {}
+  },
+  components: {
+    Header
+  }
+};
 </script>
